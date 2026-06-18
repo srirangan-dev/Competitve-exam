c37c831f468d647f24d8f83e5aa5045ed21dabe3
Sri Rangan <srirangan0603@gmail.com>
2026-06-18 21:17:28 +0530

    Update vite.config.js

--- school/vite.config.js
+++ school/vite.config.js
@@ -12,9 +12,15 @@ export default defineConfig({
   server: {
     port: 5173,
     open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
+    },
   },
   build: {
     outDir: 'dist',
-    minify: 'esbuild',  // ← CHANGED from 'terser'
+    minify: 'esbuild',
   },
 });
